3247562317890e1bdf403943764e22201fac8916
Evan-mart <martinez.evan@orange.fr>
2022-08-16 09:02:26 +0200

    first commit

---
+++ src/main/java/com/evan/lejo/lejo/LejoApplication.java
@@ -0,0 +1,13 @@
+package com.evan.lejo.lejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LejoApplication.class, args);
+	}
+
+}
